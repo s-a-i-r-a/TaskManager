b4f698a3d3c0cf6e7a2544a617350326e3be644e
Saira <137724918+s-a-i-r-a@users.noreply.github.com>
2024-01-09 18:11:27 +0000

    Update 1.0

--- Priority.java
+++
@@ -1,7 +0,0 @@
-package Utilities;
-
-public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH,
-}
---
+++ TaskManager/src/main/java/GUI/TaskManagerController.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class TaskManagerController {
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    private Button launchButton;
+
+    @FXML
+    private Button exitButton;
+
+    // You can add your event handling and other methods here
+
+
+
+
+}
